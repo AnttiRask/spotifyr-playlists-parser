27d31575024eef16973291326258af82eeca44ad
Antti Rask <antti.rask@teosto.fi>
2023-07-07 00:42:55 +0300

    My changes

--- weekly_discovery.R
+++ weekly_discovery.R
@@ -1,32 +1,66 @@
-install.packages("spotifyr")
-library(spotifyr)
+# Load libraries
 library(dplyr)
+library(lubridate)
+library(readr)
+library(spotifyr)
+library(stringr)
 
-access_token <- get_spotify_access_token(client_id = Sys.getenv("SPOTIFY_CLIENT_ID"),
-                                         client_secret = Sys.getenv("SPOTIFY_CLIENT_SECRET"))
+source("secret.R")
 
-temp <- get_playlist_tracks(playlist_id = "37i9dQZEVXcGWdRbKjgpyh") 
+# Get the SpotifyAPI Access Token ----
+Sys.setenv(SPOTIFY_CLIENT_ID     = .SPOTIFY_CLIENT_ID)
+Sys.setenv(SPOTIFY_CLIENT_SECRET = .SPOTIFY_CLIENT_SECRET)
 
-artists <- temp$track.artists %>% 
-  bind_rows(.id = "song_id") %>% 
-  slice_head(n = 1, by = "song_id") %>% 
-  select(name)
+access_token <- get_spotify_access_token()
 
-songs <- temp %>%  select(date = added_at,
-                          duration_ms = track.duration_ms, 
-                          song = track.name, 
-                          popularity = track.popularity, 
-                          release_date = track.album.release_date, 
-                          id = track.album.id) %>% 
-  mutate(date = as.Date(date))
+# Get the playlist ----
+playlist <- get_playlist_tracks(
+    playlist_id   = "37i9dQZEVXcEU5dKzCMv5W",
+    authorization = access_token
+) %>% as_tibble()
 
-final_df <- artists %>% bind_cols(songs)
-print(final_df)
+# Pull the artist names ----
+artists <- playlist %>% pull(track.artists) %>% 
+    bind_rows(.id = "song_id") %>% 
+    slice_head(n = 1, by = "song_id") %>% 
+    select(artist_name = name) %>%
+    as_tibble()
 
-day = as.character(unique(final_df$date))
+# Pull the songs ----
+songs <- playlist %>%
+    mutate(
+        date_added   = as_date(added_at),
+        duration_s   = round(track.duration_ms / 1000, 0),
+        release_year = if_else(
+            track.album.release_date_precision == "year",
+            as.numeric(track.album.release_date),
+            year(as_date(track.album.release_date))
+        )
+    ) %>%
+    select(
+        song_name    = track.name,
+        song_id      = track.id,
+        duration_s,
+        release_year,
+        popularity   = track.popularity,
+        date_added
+    )
 
-print(day)
+# Combine artists and songs ----
+artists_and_songs <- artists %>%
+    bind_cols(songs)
 
-write.csv2(x = final_df, 
-                    file = paste0("./weekly_discovers/",day,".csv"),
-                    row.names = F)
+artists_and_songs
+
+# Pull the date_added from the artists_and_songs ----
+date_added <- artists_and_songs %>%
+    distinct(date_added) %>%
+    pull()
+
+date_added
+
+# Write the csv file ----
+write_excel_csv2(
+    artists_and_songs,
+    file = str_c("weekly_discovers/", date_added, ".csv")
+)
